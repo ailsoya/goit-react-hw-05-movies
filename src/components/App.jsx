edda07d458cda9685005541dda98bbdf3f216df6
ailsoya <akalitata53@gmail.com>
2023-11-01 23:29:39 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,9 +24,7 @@ export const App = () => {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} >
-          <Route path="query=:search" element={<Reviews />} />
-        </Route>
+        <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:movieId" element={<MovieDetails />} >
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
